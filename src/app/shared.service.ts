d8987c01585d2a36cd2079c934bf9fae842982d7
chintan7786 <davdachintan97@gmail.com>
2022-11-07 02:06:02 +0530

    second commit

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SharedService {
-  readonly APIUrl = 'http://127.0.0.1:8000/'
+  readonly APIUrl = 'https://dot-product-django.herokuapp.com/'
   constructor(private http:HttpClient) { }
 
   getCardList():Observable<any[]> {
